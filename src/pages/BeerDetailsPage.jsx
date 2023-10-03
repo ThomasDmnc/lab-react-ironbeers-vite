5db29e620ce3167986795fd1ac4630008ee2a908
thomas demoncy <demoncy.thomas@gmail.com>
2023-10-03 20:40:43 +0200

    finished the lab without any styling

--- src/pages/BeerDetailsPage.jsx
+++ src/pages/BeerDetailsPage.jsx
@@ -1,3 +1,41 @@
-function BeerDetailsPage() {}
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+
+function BeerDetailsPage() {
+const { beerId } = useParams(); 
+
+const [beer, setBeer] = useState();
+const [isLoading, setIsLoading] = useState(true);
+
+async function fetchOneBeer() {
+    const response = await fetch(`https://ih-beers-api2.herokuapp.com/beers/${beerId}`)
+    console.log(response)
+    if (response.ok){
+        const parsed = await response.json()
+        console.log(parsed)
+        setBeer(parsed)
+        setIsLoading(false);
+    }
+}
+
+
+    useEffect(() => {
+        fetchOneBeer()
+    }, []);
+
+
+return isLoading ? ( <p>Loading…</p>) :  (
+    <>
+        <img src={beer.image_url} alt="" />
+        <h1>{beer.name}</h1>
+        <p>{beer.tagline}</p>
+        <p>{beer.attenuation_level}</p>
+        <p>First brewed: {beer.first_brewed}</p>
+        <p> {beer.description}</p>
+        <p>Contribution by: {beer.contributed_by}</p>
+    </>
+)
+}
 
 export default BeerDetailsPage;
--- src/pages/RandomBeerPage.jsx
+++ src/pages/RandomBeerPage.jsx
@@ -1,3 +1,39 @@
-function RandomBeersPage() {}
+import { useState, useEffect } from "react";
 
-export default RandomBeersPage;
+
+function RandomBeerPage() {
+
+const [beer, setBeer] = useState();
+const [isLoading, setIsLoading] = useState(true);
+
+async function fetchOneBeer() {
+    const response = await fetch(`https://ih-beers-api2.herokuapp.com/beers/random`)
+    console.log(response)
+    if (response.ok){
+        const parsed = await response.json()
+        console.log(parsed)
+        setBeer(parsed)
+        setIsLoading(false);
+    }
+}
+
+
+    useEffect(() => {
+        fetchOneBeer()
+    }, []);
+
+
+return isLoading ? ( <p>Loading…</p>) :  (
+    <>
+        <img src={beer.image_url} alt="" />
+        <h1>{beer.name}</h1>
+        <p>{beer.tagline}</p>
+        <p>{beer.attenuation_level}</p>
+        <p>First brewed: {beer.first_brewed}</p>
+        <p> {beer.description}</p>
+        <p>Contribution by: {beer.contributed_by}</p>
+    </>
+)
+}
+
+export default RandomBeerPage;
